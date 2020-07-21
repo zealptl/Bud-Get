938361a374bfbdd4e6db5fea26e0e0f5a508fe65
zealptl <zealpatel06@gmail.com>
2020-07-20 21:51:11 -0400

    switched from koa to express

--- src/server.js
+++ src/server.js
@@ -1,13 +1,17 @@
-const Koa = require('koa');
-const router = require('./routes');
+const express = require('express');
 const connectDB = require('../config/db');
 
-const app = new Koa();
+const app = express();
 
 connectDB();
 
-app.use(router.routes());
+app.use(express.json({ extended: false }));
+
+// app.use('/api/auth', require('./routes/auth'));
+// app.use('/api/users', require('./routes/users'));
+// app.use('/api/incomeOrExpense', require('./routes/incomeOrExpense'));
+// app.use('/api/userIncomeAndExpense', require('./routes/userIncomeAndExpense'));
 
 const PORT = 5000;
 
-app.listen(PORT);
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));

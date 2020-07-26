4fd07a977c44714c96363af6f7b1b9ad2b3e59c6
zealptl <zealpatel06@gmail.com>
2020-07-26 14:31:26 -0400

    removing userIncomeAndExpenses and putting transactions in user model

--- src/models/user.js
+++ src/models/user.js
@@ -5,6 +5,18 @@ const UserSchema = mongoose.Schema({
   lastName: String,
   email: String,
   password: String,
+  transactions: {
+    monthly: {
+      income: { type: Number, default: 0 },
+      expense: { type: Number, default: 0 },
+      savings: { type: Number, default: 0 },
+    },
+    yearly: {
+      income: { type: Number, default: 0 },
+      expense: { type: Number, default: 0 },
+      savings: { type: Number, default: 0 },
+    },
+  },
   createdOn: {
     type: Date,
     default: Date.now,
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const { check } = require('express-validator');
 const createUserMiddleware = require('./middlewares/createUser');
+
 const router = express.Router();
 
 router.post('/', createUserMiddleware);

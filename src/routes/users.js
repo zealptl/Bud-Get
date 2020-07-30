9f62309ef4749efc6d04aba934c5c4b3b4b5b0f7
zealptl <zealpatel06@gmail.com>
2020-07-30 08:44:14 -0400

    minor bug fix in users route

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,11 +3,9 @@ const authCallbackMiddleware = require('./middlewares/authCallbackMiddleware');
 const createUserMiddleware = require('./middlewares/createUser');
 const updateUserMiddleware = require('./middlewares/updateUser');
 const deleteUserMiddleware = require('./middlewares/deleteUser');
-
 const router = express.Router();
 
-router.post('/', authCallbackMiddleware, createUserMiddleware);
+router.post('/', createUserMiddleware);
 router.put('/', authCallbackMiddleware, updateUserMiddleware);
 router.delete('/', authCallbackMiddleware, deleteUserMiddleware);
-
 module.exports = router;

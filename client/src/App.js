5857e079c0624ff3ed9ae40439342d01d6bd1f0a
zealptl <zealpatel06@gmail.com>
2020-07-31 22:43:08 -0400

    creating Mui themes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import darkTheme from './theme/darkTheme';
+import lightTheme from './theme/lightTheme';
 
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className='App'>
-      <h1>Hello World</h1>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline>
+        <h1>Hello World</h1>
+      </CssBaseline>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;

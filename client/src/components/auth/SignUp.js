d08af2de43b79d4d773d19ce15068115a2e96693
zealptl <zealpatel06@gmail.com>
2020-08-09 20:46:51 -0400

    integrated backend

--- client/src/components/auth/SignUp.js
+++ client/src/components/auth/SignUp.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import {
   Avatar,
   Button,
@@ -12,6 +12,9 @@ import {
   Container,
 } from '@material-ui/core';
 import { LockOutlined } from '@material-ui/icons';
+
+import AuthContext from '../../context/auth/AuthContext';
+
 const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(10),
@@ -25,7 +28,7 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: theme.palette.primary.main,
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
+    width: '100%',
     marginTop: theme.spacing(3),
   },
   submit: {
@@ -33,8 +36,51 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const SignUp = () => {
+const SignUp = (props) => {
   const classes = useStyles();
+
+  const authContext = useContext(AuthContext);
+  const { signUpUser, error, clearError, isAuthenticated } = authContext;
+
+  const [user, setUser] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    password2: '',
+  });
+
+  const { firstName, lastName, email, password, password2 } = user;
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      props.history.push('/');
+    }
+
+    if (error === 'User already exists') {
+      console.error(error);
+    }
+  }, [error, isAuthenticated, props.history]);
+
+  const onChange = (e) => setUser({ ...user, [e.target.name]: e.target.value });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (
+      firstName === '' ||
+      lastName === '' ||
+      email === '' ||
+      password === ''
+    ) {
+      console.log('Plese enter all fields');
+    } else if (password !== password2) {
+      console.log('Passwords do nott match');
+    } else {
+      signUpUser({ firstName, lastName, email, password });
+    }
+  };
+
   return (
     <Container component='main' maxWidth='xs'>
       <CssBaseline />
@@ -46,27 +92,31 @@ const SignUp = () => {
           {' '}
           Sign Up
         </Typography>
-        <form className={classes.form}>
+        <form className={classes.form} onSubmit={onSubmit}>
           <Grid container spacing={2}>
             <Grid item xs={12} sm={6}>
               <TextField
                 name='firstName'
                 variant='outlined'
-                required
-                fullwidth
                 id='firstName'
                 label='First Name'
+                onChange={onChange}
+                value={firstName}
+                required
+                fullWidth
                 autoFocus
               />
             </Grid>
             <Grid item xs={12} sm={6}>
               <TextField
-                name='lasttName'
+                name='lastName'
                 variant='outlined'
-                required
-                fullwidth
                 id='lastName'
                 label='Last Name'
+                onChange={onChange}
+                value={lastName}
+                required
+                fullWidth
                 autoFocus
               />
             </Grid>
@@ -74,40 +124,48 @@ const SignUp = () => {
               <TextField
                 name='email'
                 variant='outlined'
-                required
-                fullWidth
                 id='email'
                 label='Email Address'
                 autoComplete='email'
+                onChange={onChange}
+                value={email}
+                required
+                fullWidth
               />
             </Grid>
             <Grid item xs={12}>
               <TextField
-                name='password1'
+                name='password'
                 variant='outlined'
+                id='password'
+                label='Password'
+                type='password'
+                onChange={onChange}
+                value={password}
                 required
                 fullWidth
-                id='password1'
-                label='Password'
               />
             </Grid>
             <Grid item xs={12}>
               <TextField
                 name='password2'
                 variant='outlined'
+                id='password2'
+                type='password'
+                label='Repeat Password'
+                onChange={onChange}
+                value={password2}
                 required
                 fullWidth
-                id='password2'
-                label='Repeat Password'
               />
             </Grid>
           </Grid>
           <Button
             type='submit'
-            fullWidth
             variant='contained'
             color='primary'
             className={classes.submit}
+            fullWidth
           >
             Sign Up
           </Button>

c9193ca3dea876b6586a1e65ecbe22c9f20cea6c
zealptl <zealpatel06@gmail.com>
2020-08-09 20:46:20 -0400

    minor style fix

--- src/routes/middlewares/getLoggedInUser.js
+++ src/routes/middlewares/getLoggedInUser.js
@@ -3,7 +3,7 @@ const UserModel = require('../../models/user');
 const getLoggedInUserMiddleware = async (req, res) => {
   try {
     const user = await UserModel.findById(req.user.id).select('-password');
-    res.json({ user });
+    res.json(user);
   } catch (error) {
     console.error(error.message);
     res.status(500).send('Server Error');

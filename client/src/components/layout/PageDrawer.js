e7ca84bb47e1ab9b31498e5088ae08dfe6688423
zealptl <zealpatel06@gmail.com>
2020-08-22 21:39:39 -0400

    Designed PageDrawer

---
+++ client/src/components/layout/PageDrawer.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import {
+  Drawer,
+  makeStyles,
+  Divider,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+} from '@material-ui/core';
+
+import {
+  DashboardRounded as DashboardRoundedIcon,
+  AddBoxRounded as AddBoxRoundedIcon,
+  MonetizationOnRounded as MonetizationOnRoundedIcon,
+  CreditCardRounded as CreditCardRoundedIcon,
+  ShowChartRounded as ShowChartRoundedIcon,
+  FavoriteRounded as FavoriteRoundedIcon,
+  ChevronRightRounded as ChevronRightRoundedIcon,
+} from '@material-ui/icons';
+
+const DRAWER_WIDTH = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    width: DRAWER_WIDTH,
+  },
+  divider: {
+    marginTop: '50px',
+  },
+}));
+
+const PageDrawer = () => {
+  const classes = useStyles();
+
+  return (
+    <Drawer variant='permanent' anchor='left' className={classes.root}>
+      <List>
+        <Divider className={classes.divider} />
+
+        <ListItem button key='Dashboard'>
+          <ListItemIcon>
+            <DashboardRoundedIcon />
+          </ListItemIcon>
+          <ListItemText primary='Dashboard' />
+        </ListItem>
+        <ListItem button key='Quick Add'>
+          <ListItemIcon>
+            <AddBoxRoundedIcon />
+          </ListItemIcon>
+          <ListItemText primary='Quick Add' />
+        </ListItem>
+        <ListItem button key='Income'>
+          <ListItemIcon>
+            <MonetizationOnRoundedIcon />
+          </ListItemIcon>
+          <ListItemText primary='Income' />
+        </ListItem>
+        <ListItem button key='Expense'>
+          <ListItemIcon>
+            <CreditCardRoundedIcon />
+          </ListItemIcon>
+          <ListItemText primary='Expense' />
+        </ListItem>
+        <ListItem button key='Trends'>
+          <ListItemIcon>
+            <ShowChartRoundedIcon />
+          </ListItemIcon>
+          <ListItemText primary='Trends' />
+        </ListItem>
+        <ListItem button key='Wishlist'>
+          <ListItemIcon>
+            <FavoriteRoundedIcon />
+          </ListItemIcon>
+          <ListItemText primary='Wishlist' />
+        </ListItem>
+      </List>
+    </Drawer>
+  );
+};
+
+export default PageDrawer;
